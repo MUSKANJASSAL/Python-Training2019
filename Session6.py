b85f0891634c437c72e77bddb22421e2a68ca94b
Jassal <muskan124.jassal@gmail.com>
2019-06-20 17:00:28 +0530

    Initial Commit

---
+++ Session6.py
@@ -0,0 +1,40 @@
+# FUNCTIONS
+# f(x) = x*x
+# f(2) = 2*2 = 4
+# METHODS/ PROCEDURES/ ROUTINES
+"""
+    FUNCTION
+    1. Name
+    2. Inputs (May or May Not) | Parameters | Arguments
+    3. return (May or May Not) | Acknowledgement
+    4. Definition (May or May Not) -> Sequence
+
+    LOOP
+        Where we write statements which are repeated again and again
+
+    Why Functions ?
+        Group of Statements / logic which has to be executed again and again
+
+    Modularization is something which we are doing in our program to simplify the process
+"""
+
+# Definition of Function: addNumbers
+# num1 and num2 are inputs
+def addNumbers(num1, num2):
+    num3 = num1 + num2
+    # print("Sum of {} and {} is {}".format(num1, num2, num3))
+# Execution of addNumbers Function
+addNumbers(10, 20)
+#addNumbers(20, 33)
+#addNumbers(45, 78)
+#addNumbers(-12, 76)
+
+"""
+def addNumbers(num1, num2):
+    num3= num1 + num2
+    return num3
+result = addNumbers(10, 20)
+print("Sum is:",result)
+print(addNumbers(20, 33))
+print("Sum is:",addNumbers(45, 78))
+"""
